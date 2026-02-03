dd30058f87ac63cce1a072aa5ef30170f9d95d6d
rsetia23 <rahulsetia2005@gmail.com>
2026-02-03 16:55:39 -0500

    Bootstrap and Tailwind parts added

--- app/labs/lab2/page.tsx
+++ app/labs/lab2/page.tsx
@@ -11,10 +11,26 @@ import Positions from "./Positions";
 import Zindex from "./Zindex";
 import Float from "./Float";
 import GridLayout from "./GridLayout";
+import Flex from "./Flex";
+import MediaQueriesDemo from "./MediaQueriesDemo";
+import ReactIconsSampler from "./ReactIconsSampler";
+import { Container } from "react-bootstrap";
+import BootstrapGrids from "./BootstrapGrids";
+import ScreenSizeLabel from "./ScreenSizeLabel";
+import BootstrapTables from "./BootstrapTables";
+import BootstrapLists from "./BootstrapLists";
+import BootstrapForms from "./BootstrapForms";
+// import TailwindLab from "./tailwind/page";
+// import TailwindSpacing from "./tailwind/TailwindSpacing";
+// import TailwindTypography from "./tailwind/TailwindTypography";
+// import TailwindBackgroundColors from "./tailwind/TailwindBackgroundColors";
+// import TailwindResponsiveDesign from "./tailwind/TailwindResponsiveDesign";
+// import TailwindFilters from "./tailwind/TailwindFilters";
 
 export default function Lab2() {
   return (
-    <div id="wd-lab2">
+    // <div id="wd-lab2"></div>
+    <Container>
       <h2>Lab 2 - Cascading Style Sheets</h2>
       <h3>Styling with the STYLE attribute</h3>
       <p>
@@ -81,6 +97,22 @@ export default function Lab2() {
       <Zindex />
       <Float />
       <GridLayout />
-    </div>
+      <Flex />
+      <MediaQueriesDemo />
+
+      <ReactIconsSampler />
+      <BootstrapGrids />
+      <ScreenSizeLabel />
+      <BootstrapTables />
+      <BootstrapLists />
+      <BootstrapForms />
+
+      {/* <TailwindLab />
+      <TailwindSpacing />
+      <TailwindTypography />
+      <TailwindBackgroundColors />
+      <TailwindResponsiveDesign />
+      <TailwindFilters /> */}
+    </Container>
   );
 }
