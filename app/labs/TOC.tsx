bc8c433e7bceff0e353cb37ccfe35bff5473ccc4
rsetia23 <rahulsetia2005@gmail.com>
2026-02-27 19:31:41 -0500

    fixed TOC.tsx

--- app/labs/TOC.tsx
+++ app/labs/TOC.tsx
@@ -44,12 +44,23 @@ export default function TOC() {
         </NavLink>{" "}
       </NavItem>
       <NavItem>
+        <NavLink
+          href="/labs/lab4"
+          as={Link}
+          className={`nav-link ${pathname.endsWith("lab3") ? "active" : ""}`}
+        >
+          Lab 4{" "}
+        </NavLink>{" "}
+      </NavItem>
+      <NavItem>
         <NavLink href="/" as={Link}>
           Kambaz{" "}
         </NavLink>{" "}
       </NavItem>
       <NavItem>
-        <NavLink href="https://github.com/jannunzi">My GitHub</NavLink>
+        <NavLink href="https://github.com/rsetia23/kambaz-next-js">
+          My GitHub
+        </NavLink>
       </NavItem>
     </Nav>
   );
