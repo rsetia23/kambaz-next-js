2cc468f5a7b1dcfbb1a5058293edd7cd827a9e4f
rsetia23 <rahulsetia2005@gmail.com>
2026-02-27 19:47:31 -0500

    fix

--- app/labs/lab4/redux/page.tsx
+++ app/labs/lab4/redux/page.tsx
@@ -3,15 +3,19 @@ import AddRedux from "./AddRedux";
 import CounterRedux from "./CounterRedux";
 import HelloRedux from "./hello";
 import TodoList from "./todos/TodoList";
+import { Provider } from "react-redux";
+import store from "../store";
 
 export default function ReduxExamples() {
  return (
-   <div>
-     <h2>Redux Examples</h2>
-     <HelloRedux />
-     <CounterRedux />
-     <AddRedux />
-     <TodoList />
-   </div>
+   <Provider store={store}>
+     <div>
+       <h2>Redux Examples</h2>
+       <HelloRedux />
+       <CounterRedux />
+       <AddRedux />
+       <TodoList />
+     </div>
+   </Provider>
  );
 }
